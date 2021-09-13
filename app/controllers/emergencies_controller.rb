adb6dbc408d1c60451757706ab3d038c4aa4c255
jmart6784 <jmartinez6784@yahoo.com>
2021-09-12 22:43:04 -0700

    Created emergency create action and passed basic creation assertion

--- app/controllers/emergencies_controller.rb
+++ app/controllers/emergencies_controller.rb
@@ -5,7 +5,45 @@ class EmergenciesController < ApplicationController
     render json: emergencies
   end
 
+  def create
+    if forbidden_param?("create")
+      unpermitted_param_response("create")
+    else
+      emergency = Emergency.new(emergency_params)
+
+      if emergency.save
+        render json: emergency, status: 201
+      else
+        render json: emergency.errors, status: 422
+      end
+    end
+  end
+
   private
+
+  def forbidden_param?(action)
+    if action === "create"
+      true if (
+        emergency_params[:id]
+      )
+    elsif action === "update"
+      true if (
+        emergency_params[:id]
+      )
+    end
+  end
+
+  def unpermitted_param_response(action)
+    if action === "create"
+      if emergency_params[:id]
+        render json: { :message => 'found unpermitted parameter: id' }, status: 422
+      end   
+    elsif action === "update"
+      if emergency_params[:id]
+        render json: { :message => 'found unpermitted parameter: id' }, status: 422
+      end      
+    end
+  end
 
   def emergency
     @emergency_code ||= Emergency.find(params[:id])
