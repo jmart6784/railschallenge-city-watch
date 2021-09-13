c4ec27bc062a8d03f39f4b1aaed4eafb81fd6e2b
jmart6784 <jmartinez6784@yahoo.com>
2021-09-12 22:50:38 -0700

    Added validtions for all severities to be greater than 0

--- app/models/emergency.rb
+++ app/models/emergency.rb
@@ -1,3 +1,15 @@
 class Emergency < ActiveRecord::Base
   has_many :responders
+
+  validates_numericality_of :fire_severity, 
+    greater_than_or_equal_to: 0, 
+    message: 'must be greater than or equal to 0'
+
+  validates_numericality_of :police_severity, 
+    greater_than_or_equal_to: 0, 
+    message: 'must be greater than or equal to 0'
+
+  validates_numericality_of :medical_severity, 
+    greater_than_or_equal_to: 0, 
+    message: 'must be greater than or equal to 0'
 end
--- test/api/emergencies_create_test.rb
+++ test/api/emergencies_create_test.rb
@@ -14,23 +14,21 @@ class EmergenciesCreateTest < ActionDispatch::IntegrationTest
     assert_equal 3, json_response['medical_severity']
   end
 
-  # test 'POST /emergencies/ all severities must be greater than or equal to zero' do
-  #   post '/emergencies', emergency: { code: 'E-55555555', fire_severity: -1, police_severity: -1, medical_severity: -1 }
-  #   assert_equal 422, response.status
-  #   assert_equal(
-  #     {
-  #       'message' => {
-  #         'fire_severity' => ['must be greater than or equal to 0'],
-  #         'police_severity' => ['must be greater than or equal to 0'],
-  #         'medical_severity' => ['must be greater than or equal to 0']
-  #       }
-  #     },
-  #     JSON.parse(body)
-  #   )
-  # end
+  test 'POST /api/v1/emergencies/ all severities must be greater than or equal to zero' do
+    post '/api/v1/emergencies', emergency: { code: 'E-55555555', fire_severity: -1, police_severity: -1, medical_severity: -1 }
+    assert_equal 422, response.status
+    assert_equal(
+      {
+        'fire_severity' => ['must be greater than or equal to 0'],
+        'police_severity' => ['must be greater than or equal to 0'],
+        'medical_severity' => ['must be greater than or equal to 0']
+      },
+      JSON.parse(body)
+    )
+  end
 
-  # test 'POST /emergencies/ code attribute must be unique' do
-  #   post '/emergencies', emergency: { code: 'E-not-unique', fire_severity: 1, police_severity: 3, medical_severity: 5 }
+  # test 'POST /api/v1/emergencies/ code attribute must be unique' do
+  #   post '/api/v1/emergencies', emergency: { code: 'E-not-unique', fire_severity: 1, police_severity: 3, medical_severity: 5 }
   #   json_response = JSON.parse(body)
 
   #   assert_equal 201, response.status
@@ -40,21 +38,21 @@ class EmergenciesCreateTest < ActionDispatch::IntegrationTest
   #   assert_equal(3, json_response['emergency']['police_severity'])
   #   assert_equal(5, json_response['emergency']['medical_severity'])
 
-  #   post '/emergencies', emergency: { code: 'E-not-unique', fire_severity: 1, police_severity: 3, medical_severity: 5 }
+  #   post '/api/v1/emergencies', emergency: { code: 'E-not-unique', fire_severity: 1, police_severity: 3, medical_severity: 5 }
 
   #   assert_equal 422, response.status
   #   assert_equal({ 'message' => { 'code' => ['has already been taken'] } }, JSON.parse(body))
   # end
 
-  # test 'POST /emergencies/ cannot set id' do
-  #   post '/emergencies', emergency: { id: 1, fire_severity: 1, police_severity: 2, medical_severity: 3 }
+  # test 'POST /api/v1/emergencies/ cannot set id' do
+  #   post '/api/v1/emergencies', emergency: { id: 1, fire_severity: 1, police_severity: 2, medical_severity: 3 }
 
   #   assert_equal 422, response.status
   #   assert_equal({ 'message' => 'found unpermitted parameter: id' }, JSON.parse(body))
   # end
 
-  # test 'POST /emergencies/ cannot set resolved_at' do
-  #   post '/emergencies',
+  # test 'POST /api/v1/emergencies/ cannot set resolved_at' do
+  #   post '/api/v1/emergencies',
   #        emergency: {
   #          resolved_at: Time.zone.now,
   #          code: 'E-55555555',
@@ -67,29 +65,29 @@ class EmergenciesCreateTest < ActionDispatch::IntegrationTest
   #   assert_equal({ 'message' => 'found unpermitted parameter: resolved_at' }, JSON.parse(body))
   # end
 
-  # test 'POST /emergencies/ lack of fire_severity returns an error' do
-  #   post '/emergencies', emergency: { code: 'E-55555555', police_severity: 2, medical_severity: 3 }
+  # test 'POST /api/v1/emergencies/ lack of fire_severity returns an error' do
+  #   post '/api/v1/emergencies', emergency: { code: 'E-55555555', police_severity: 2, medical_severity: 3 }
 
   #   assert_equal 422, response.status
   #   assert_equal({ 'message' => { 'fire_severity' => ['can\'t be blank', 'is not a number'] } }, JSON.parse(body))
   # end
 
-  # test 'POST /emergencies/ lack of police_severity returns an error' do
-  #   post '/emergencies', emergency: { code: 'E-55555555', fire_severity: 2, medical_severity: 3 }
+  # test 'POST /api/v1/emergencies/ lack of police_severity returns an error' do
+  #   post '/api/v1/emergencies', emergency: { code: 'E-55555555', fire_severity: 2, medical_severity: 3 }
 
   #   assert_equal 422, response.status
   #   assert_equal({ 'message' => { 'police_severity' => ['can\'t be blank', 'is not a number'] } }, JSON.parse(body))
   # end
 
-  # test 'POST /emergencies/ lack of medical_severity returns an error' do
-  #   post '/emergencies', emergency: { code: 'E-55555555', fire_severity: 2, police_severity: 3 }
+  # test 'POST /api/v1/emergencies/ lack of medical_severity returns an error' do
+  #   post '/api/v1/emergencies', emergency: { code: 'E-55555555', fire_severity: 2, police_severity: 3 }
 
   #   assert_equal 422, response.status
   #   assert_equal({ 'message' => { 'medical_severity' => ['can\'t be blank', 'is not a number'] } }, JSON.parse(body))
   # end
 
-  # test 'POST /emergencies/ lack of multiple required fields returns an error' do
-  #   post '/emergencies', emergency: { fire_severity: 1 }
+  # test 'POST /api/v1/emergencies/ lack of multiple required fields returns an error' do
+  #   post '/api/v1/emergencies', emergency: { fire_severity: 1 }
 
   #   assert_equal 422, response.status
   #   assert_equal(
